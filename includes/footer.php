f1d83249426ef6a7fb4d7cba67003a154625ec16
pacifictraining2025 <kiran.more@1page.info>
2026-04-13 02:03:39 +0530

    updated header things

---
+++ includes/footer.php
@@ -0,0 +1,93 @@
+<!-- Main Footer -->
+<footer class="main-footer">
+	<div class="main-footer_bg" style="background: linear-gradient(135deg, #0f3f73 0%, #1463B6 50%, #38BDF8 100%);"></div>
+	<div class="auto-container">
+		<!-- Widgets Section -->
+		<div class="widgets-section">
+			<div class="row clearfix">
+
+				<!-- Column 1: Logo & Company Info -->
+				<div class="main-footer_column col-lg-3 col-md-6 col-sm-12">
+					<div class="logo-widget">
+						<div class="main-footer_logo"><a href="/vhs_website/index.html"><img src="/vhs_website/images/LOGO.png" alt="VHS Engineering" title="VHS Engineering"></a></div>
+						<div class="main-footer_text">ISO 9001-2015 certified structural steel design and detailing company delivering world-class engineering solutions across diverse project types.</div>
+						<!-- Social Box -->
+						<div class="main-footer_socials">
+							<span>Follow Us:</span>
+							<a href="https://www.facebook.com/Vilas-Suryawanshi-108669764340120/" target="_blank"><i class="fa-brands fa-facebook-f"></i></a>
+							<a href="https://www.linkedin.com/in/vhs-engineering-70272417a/" target="_blank"><i class="fa-brands fa-linkedin-in"></i></a>
+							<a href="https://www.youtube.com/" target="_blank"><i class="fa-brands fa-youtube"></i></a>
+							<a href="https://www.instagram.com/" target="_blank"><i class="fa-brands fa-instagram"></i></a>
+						</div>
+					</div>
+				</div>
+
+				<!-- Column 2: Services -->
+				<div class="main-footer_column col-lg-3 col-md-6 col-sm-12">
+					<div class="links-widget">
+						<h4 class="main-footer_title">Services</h4>
+						<ul class="main-footer_links">
+							<li><a href="/vhs_website/structural-design-and-consultancy.html">Structural Design & Consultancy</a></li>
+							<li><a href="/vhs_website/bim-services.html">BIM Services</a></li>
+							<li><a href="/vhs_website/Estimodelling-and-Pre-Bid-Services.html">Estimodelling & Pre-Bid Services</a></li>
+							<li><a href="/vhs_website/Connection-Design-Services.html">Connection Design Services</a></li>
+							<li><a href="/vhs_website/Steel-Detailing-Services.html">Steel Detailing Services</a></li>
+							<li><a href="/vhs_website/Deputation-Services.html">Deputation Services</a></li>
+							<li><a href="/vhs_website/Other-Engineering-Services.html">Other Engineering Services</a></li>
+						</ul>
+					</div>
+				</div>
+
+				<!-- Column 3: Useful Links -->
+				<div class="main-footer_column col-lg-3 col-md-6 col-sm-12">
+					<div class="links-widget">
+						<h4 class="main-footer_title">Useful Links</h4>
+						<ul class="main-footer_links">
+							<li><a href="https://www.nisd.org/" target="_blank">NISD</a></li>
+							<li><a href="https://www.aisc.org/" target="_blank">AISC</a></li>
+							<li><a href="https://www.cisc-icca.ca/" target="_blank">CISC</a></li>
+							<li><a href="https://www.steel.org.au/" target="_blank">Steel Australia</a></li>
+							<li><a href="https://www.tekla.com/in" target="_blank">Tekla</a></li>
+							<li><a href="https://www.autodesk.com/" target="_blank">Autodesk</a></li>
+						</ul>
+					</div>
+				</div>
+
+				<!-- Column 4: Contact Us -->
+				<div class="main-footer_column col-lg-3 col-md-6 col-sm-12">
+					<div class="contact-widget">
+						<h4 class="main-footer_title">Contact Us</h4>
+						<ul class="main-footer_links">
+							<li>
+								<span class="contact-icon"><i class="fa-solid fa-phone fa-fw"></i></span>
+								<a href="tel:+917588639200">+91 7588639200</a>
+							</li>
+							<li>
+								<span class="contact-icon"><i class="fa-solid fa-location-dot fa-fw"></i></span>
+								<span>Omkar, Unit#301, 3rd floor, Maulana Azad Road, Cross Phadke Road, Subhas Nagar, Dombivli(East), Thane - 421 201, Maharashtra</span>
+							</li>
+							<li>
+								<span class="contact-icon"><i class="fa-solid fa-envelope fa-fw"></i></span>
+								<a href="mailto:contact@vhsengg.com">contact@vhsengg.com</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+
+			</div>
+		</div>
+		
+		<div class="main-footer_bottom">
+			<div class="d-flex justify-content-between align-items-center flex-wrap">
+				<!-- Copyright -->
+				<div class="main-footer_copyright">&copy; 2026 VHS Engineering Pvt. Ltd. All rights Reserved.</div>
+				<ul class="main-footer_navs">
+					<li><a href="/vhs_website/about.html">About Us</a></li>
+					<li><a href="/vhs_website/contact.html">Contact Us</a></li>
+				</ul>
+			</div>
+		</div>
+		
+	</div>
+</footer>
+<!-- End Main Footer -->
---
+++ includes/header.php
@@ -0,0 +1,84 @@
+<!-- Main Header -->
+<header class="main-header header-style-one">
+	
+	<!-- Header Lower -->
+	<div class="header-lower">
+		<div class="auto-container">
+			<div class="inner-container">
+				<div class="d-flex justify-content-between align-items-center">
+					
+					<div class="logo-box">
+						<div class="logo"><a href="/vhs_website/index.html"><img src="/vhs_website/images/LOGO.png" alt="VHS Engineering" title="VHS Engineering"></a></div>
+					</div>
+					
+					<div class="nav-outer d-flex flex-wrap">
+						<!-- Main Menu -->
+						<nav class="main-menu navbar-expand-md">
+							<div class="navbar-header">
+								<!-- Toggle Button -->
+								<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+								</button>
+							</div>
+							
+							<div class="navbar-collapse collapse clearfix" id="navbarSupportedContent">
+								<ul class="navigation clearfix">
+									<li><a href="/vhs_website/index.html">Home</a></li>
+									<li><a href="/vhs_website/about.html">About</a></li>
+									<li class="dropdown"><a href="/vhs_website/services.html">Services</a>
+										<ul>
+											<li><a href="/vhs_website/structural-design-and-consultancy.html">Structural Design & Consultancy</a></li>
+											<li><a href="/vhs_website/bim-services.html">BIM Services</a></li>
+											<li><a href="/vhs_website/Estimodelling-and-Pre-Bid-Services.html">Estimodelling & Pre-Bid Services</a></li>
+											<li><a href="/vhs_website/Connection-Design-Services.html">Connection Design Services</a></li>
+											<li><a href="/vhs_website/Steel-Detailing-Services.html">Steel Detailing Services</a></li>
+											<li><a href="/vhs_website/Deputation-Services.html">Deputation Services</a></li>
+											<li><a href="/vhs_website/Other-Engineering-Services.html">Other Engineering Services</a></li>
+										</ul>
+									</li>
+									<li><a href="/vhs_website/projects.html">Projects</a></li>
+									<li><a href="/vhs_website/blog.html">Blog</a></li>
+									<li><a href="/vhs_website/contact.html">Contact</a></li>
+								</ul>
+							</div>
+						</nav>
+					</div>
+
+					<!-- Main Menu End-->
+					<div class="outer-box d-flex align-items-center flex-wrap">
+						<!-- Button Box -->
+						<div class="main-header_button">
+							<a href="/vhs_website/contact.html" class="theme-btn btn-style-three">
+								<span class="btn-wrap">
+									<span class="text-one">GET A QUOTE <i><img src="/vhs_website/assets/images/icons/arrow-1.svg" alt="" /></i></span>
+									<span class="text-two">GET A QUOTE <i><img src="/vhs_website/assets/images/icons/arrow-1.svg" alt="" /></i></span>
+								</span>
+							</a>
+						</div>
+						
+						<!-- Mobile Navigation Toggler -->
+						<div class="mobile-nav-toggler"><span class="icon fa-classic fa-solid fa-bars fa-fw"></span></div>
+					</div>
+					
+				</div>
+			</div>
+		</div>
+	</div>
+	<!--End Header Lower-->
+	
+	<!-- Mobile Menu  -->
+	<div class="mobile-menu">
+		<div class="menu-backdrop"></div>
+		<div class="close-btn"><span class="icon fa-xmark"></span></div>
+		
+		<nav class="menu-box">
+			<div class="nav-logo"><a href="/vhs_website/index.html"><img src="/vhs_website/assets/images/mobile-logo.svg" alt="" title=""></a></div>
+			<div class="menu-outer"><!--Here Menu Will Come Automatically Via Javascript / Same Menu as in Header--></div>
+		</nav>
+	</div>
+	<!-- End Mobile Menu -->
+
+</header>
+<!-- End Main Header -->
